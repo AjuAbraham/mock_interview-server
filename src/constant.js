932424f89cb78cdc5679debf76a9dc93519cee54
Aju Abraham <ajubabraham23@gmail.com>
2024-07-14 12:52:17 +0530

    Setup server

--- src/constant.js
+++
@@ -1,5 +0,0 @@
-export const DB_NAME = "mock"
-
-
-
-
--- src/utils/AsyncHandler.js
+++
@@ -1,9 +0,0 @@
- const AsyncHandler = (asyncFunction) => async(req,res,next)=>{
-     try {
-       await asyncFunction(req,res,next); 
-     } catch (error) {
-        next(error)
-     }
-}
-
-export default AsyncHandler;
