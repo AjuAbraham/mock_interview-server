48a508f2b784dad913c4452f10d2aaf0c3c64a94
Aju Abraham <ajubabraham23@gmail.com>
2024-07-15 23:08:54 +0530

    Create handlers for controller

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
 import express from "express";
-import dotenv from 'dotenv';
-import cors from 'cors';
+import dotenv from "dotenv";
+import cors from "cors";
+
 
 dotenv.config();
 const app = express();
-app.use(cors({
-    origin: process.env.ORIGIN
-}));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
 // Config middlewares
 app.use(express.json({ limit: "20kb" }));
@@ -20,11 +23,22 @@ export const server = createServer(app);
 let io;
 
 export const initializeSocket = () => {
-    io = new Server(server, {
-        cors: true
-    });
+  io = new Server(server, {
+    cors: true,
+  });
 
-    io.on("connection", () => {
-        console.log("connected");
-    });
+  io.on("connection", () => {
+    console.log("connected");
+  });
 };
+
+//routes
+
+// error handler
+app.use((err, _, res ) => {
+  const statusCode = err.statusCode || 500;
+  return res.status(statusCode).json({
+    message: err.message,
+    success: false,
+  })
+});
