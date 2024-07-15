0456f357433cd284dea7006a9536b15f367c03e5
Aju Abraham <ajubabraham23@gmail.com>
2024-07-15 19:41:53 +0530

    Socket setup done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("hello");
+import { Server } from "socket.io";
+import { createServer } from "http";
+import express from "express";
+import dotenv from 'dotenv'
+
+
+dotenv.config("./.env")
+const app = express();
+const server = createServer(app);
+const io = new Server(server,{
+    cors:true
+});
+
+io.on("connection",()=>{
+    console.log("connected")
+})
+
+
+
+
+server.listen(process.env.PORT,()=>{
+    console.log(`server is running on port: ${process.env.PORT||8000}`)
+})  
