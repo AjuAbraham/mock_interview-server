d596c5b6d9cb4cf30ccb63053d547546ad479860
Aju Abraham <ajubabraham23@gmail.com>
2024-07-16 12:53:02 +0530

    Create password hash and jwt function

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,34 +1,52 @@
+/* eslint-disable no-undef */
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      required: [true,"username is required"],
-      lowercase:true,
-      trim:true, 
-      index:true
+      required: [true, "username is required"],
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     email: {
       type: String,
       unique: true,
-      lowercase:true,
-      required: [true,"email is required"],
-      
+      lowercase: true,
+      required: [true, "email is required"],
     },
     password: {
       type: String,
       unique: true,
-      required: [true,"password is required"],
+      required: [true, "password is required"],
     },
     feedback: [
-{
-    type: Schema.Types.ObjectId,
-    ref:"Feedback"
-}
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Feedback",
+      },
     ],
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  }
+  return next();
+});
+
+userSchema.methods.checkPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccess = function () {
+  return jwt.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  });
+};
 
 export const User = mongoose.model("User", userSchema);
